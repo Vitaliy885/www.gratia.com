cb66b54a4acec8676b2c503eda60a260a5b5f2b2
Vitaliy Kapanyuk <Vitalik885@gmail.com>
2016-11-24 22:22:10 +0200

    new carousel

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -3,28 +3,47 @@ $(document).ready(function(){
     $('.trigger-victim').slideToggle(500);
   });
   $(window).resize(function(){
-      if($(window).width() > 992){
-        $('.trigger-victim').removeAttr('style');
-      }
-    })
+    if($(window).width() > 992){
+      $('.trigger-victim').removeAttr('style');
+    }
+  })
 
   $(window).scroll(function(){
-  	 var height = $(document).scrollTop();
-  	 if(height > 500){
-  	 	$(".btn-up").fadeIn(500);
-  	 }
-  	 else{
-  	 	$(".btn-up").fadeOut(500);
-  	 }
-  });
+    var height = $(document).scrollTop();
+    if(height > 500){
+     $(".btn-up").fadeIn(500);
+   }
+   else{
+     $(".btn-up").fadeOut(500);
+   }
+ });
 })
 $(document).ready(function(){
   $('.btn-up').mPageScroll2id();
 })
 
+$(document).ready(function() {
+
+  $("#owl-demo").owlCarousel({
+      
+      autoPlay: 5000,
+      navigation : true, // Show next and prev buttons
+      slideSpeed : 500,
+      paginationSpeed : 400,
+      singleItem:true
+
+      // "singleItem:true" is a shortcut for:
+      // items : 1, 
+      // itemsDesktop : false,
+      // itemsDesktopSmall : false,
+      // itemsTablet: false,
+      // itemsMobile : false
+
+    });
+
+});
 
 
 
 
-    
- 
+
